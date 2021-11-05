a998776c0dde110400725710790127135fc3699b
Cameron Tong <cameron.tong2b@gmail.com>
2021-11-05 14:09:27 -0400

    feat: implement aws s3 bucket for image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,12 @@ class Micropost < ApplicationRecord
   validates(:content, presence: true, length: {maximum: 300})
   validates(:user_id, presence: true)
   default_scope -> {order(created_at: :desc)}
+  validates(:image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                            message: 'must be a valid format'},
+                    size: {less_than: 5.megabytes,
+                            message: 'should be less than 5MB'})
+
+  def display_image
+    image.variant(resize_to_limit: [500,500])
+  end
 end
