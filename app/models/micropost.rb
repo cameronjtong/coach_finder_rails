867fe11ebd994db26b1aa240be4d7570b3f3cc46
Cameron Tong <cameron.tong2b@gmail.com>
2021-11-05 13:37:58 -0400

    feat: implement image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   validates(:content, presence: true, length: {maximum: 300})
   validates(:user_id, presence: true)
   default_scope -> {order(created_at: :desc)}
